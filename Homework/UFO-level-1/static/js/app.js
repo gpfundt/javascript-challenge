b5669b4e5147a9199ec9759264782f5ab98b8ab4
Greta Pfundt <g.pfundt325@gmail.com>
2019-12-02 20:05:39 -0500

    UFO-level-1

---
+++ Homework/UFO-level-1/static/js/app.js
@@ -0,0 +1,37 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(function(alienData) {
+    //console.log(alienData);
+    var row = tbody.append("tr");
+    Object.entries(alienData).forEach(function([key, value]) {
+      //console.log(key, value);
+
+      var cell = row.append("td");
+      cell.text(value);
+      //console.log(value);
+    });
+  });
+
+const button = d3.select("#filter-btn");
+button.on("click", function(filterbtn) {
+      // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+      // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = data.filter(x => x.datetime === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach(function(filteredAliens) {
+        var row = tbody.append("tr");
+        Object.entries(filteredAliens).forEach(function([key, value]) {
+          var cell = row.append("td");
+          cell.text(value);
+          //console.log(value);
+        });
+      });
+});
